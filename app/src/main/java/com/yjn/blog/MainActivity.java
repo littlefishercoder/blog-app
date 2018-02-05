2689b5064f0dc0699b0c2dd2fa2cb0f7f7c8300e
yang.jianan <364804051@qq.com>
2018-02-05 17:48:26 +0800

    基础框架

---
+++ app/src/main/java/com/yjn/blog/MainActivity.java
@@ -0,0 +1,21 @@
+package com.yjn.blog;
+
+import com.yjn.common.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
